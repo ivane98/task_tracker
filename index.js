35400b6ab892c41936499598040a9a7b0c260feb
ivane98 <i.vardoshvili22@gmail.com>
2025-08-19 09:26:42 +0400

    added update and delete

--- index.js
+++ index.js
@@ -12,9 +12,54 @@ export function writeToJSON(newData) {
   }
 
   // Add new data
-  fileData.push({ task: newData, id: fileData.length + 1 });
+  const sortedTasks = fileData.slice().sort((a, b) => {
+    let x = a.id < b.id ? 1 : -1;
+    return x;
+  });
+
+  const taskId = sortedTasks[0] ? sortedTasks[0]["id"] + 1 : 1;
+
+  fileData.push({
+    id: taskId,
+    description: newData,
+    status: "todo",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
   console.log(fileData);
-  console.log(fileData.length);
+
+  // Write back to file
+  fs.writeFileSync(filePath, JSON.stringify(fileData, null, 2), "utf-8");
+}
+
+export function updateJSON(newData, id) {
+  const filePath = "./tasks.json";
+
+  // Read existing data
+  let fileData = [];
+  if (fs.existsSync(filePath)) {
+    const raw = fs.readFileSync(filePath, "utf-8");
+    fileData = { raw } ? JSON.parse(raw) : [];
+  }
+
+  let taskToUpdate = fileData.find((task) => task.id === id);
+  taskToUpdate.task = newData;
+
+  // Write back to file
+  fs.writeFileSync(filePath, JSON.stringify(fileData, null, 2), "utf-8");
+}
+
+export function deleteFromJSON(id) {
+  const filePath = "./tasks.json";
+
+  // Read existing data
+  let fileData = [];
+  if (fs.existsSync(filePath)) {
+    const raw = fs.readFileSync(filePath, "utf-8");
+    fileData = { raw } ? JSON.parse(raw) : [];
+  }
+
+  fileData = fileData.filter((task) => task.id !== id);
 
   // Write back to file
   fs.writeFileSync(filePath, JSON.stringify(fileData, null, 2), "utf-8");
@@ -26,7 +71,23 @@ const rl = readline.createInterface({
 });
 
 rl.question(`task-tracker `, (input) => {
-  const task = input.split('"')[1];
-  writeToJSON(task);
+  let command;
+  if (input.split('"')[0].trim().split(" ").length > 1) {
+    command = input.split('"')[0].trim().split(" ")[0];
+  } else {
+    command = input.split('"')[0].trim();
+  }
+  if (command === "add") {
+    const task = input.split('"')[1];
+    writeToJSON(task);
+  } else if (command === "update") {
+    const id = parseInt(input.split('"')[0].trim().split(" ")[1]);
+    const task = input.split('"')[1];
+    updateJSON(task, id);
+  } else if (command === "delete") {
+    const id = parseInt(input.split('"')[0].trim().split(" ")[1]);
+    deleteFromJSON(id);
+  }
+
   rl.close();
 });
